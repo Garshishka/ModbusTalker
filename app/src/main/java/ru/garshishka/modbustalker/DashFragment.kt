9947ede74c557db53c11c55c6bd51f2cf50beedc
garshishka <garshiv@gmail.com>
2023-12-26 23:49:49 +0300

    Added reading answer from server

--- app/src/main/java/ru/garshishka/modbustalker/DashFragment.kt
+++ app/src/main/java/ru/garshishka/modbustalker/DashFragment.kt
@@ -10,10 +10,13 @@ import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import io.ktor.network.selector.SelectorManager
 import io.ktor.network.sockets.aSocket
+import io.ktor.network.sockets.openReadChannel
 import io.ktor.network.sockets.openWriteChannel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import ru.garshishka.modbustalker.databinding.FragmentDashBinding
+import kotlin.system.exitProcess
 
 class DashFragment : Fragment() {
     private val binding: FragmentDashBinding by viewBinding(createMethod = CreateMethod.INFLATE)
@@ -42,8 +45,8 @@ class DashFragment : Fragment() {
             try {
                 val socket = aSocket(selectorManager).tcp().connect("10.0.2.2", 502)
                 Log.d("ModBus", "connected")
-                //val receiveChannel = socket.openReadChannel()
-                val output = socket.openWriteChannel(autoFlush = true)
+                val receiveChannel = socket.openReadChannel()
+                val sendChannel  = socket.openWriteChannel(autoFlush = true)
 
                 Log.d("ModBus", "sending to socket")
                 val packet = byteArrayOfInts(
@@ -60,8 +63,27 @@ class DashFragment : Fragment() {
                     0xff,
                     0x00
                 )
-                output.writeFully(packet, 0, 12)
-                socket.close()
+                sendChannel.writeFully(packet, 0, 12)
+
+                launch(Dispatchers.IO) {
+                    while (true) {
+                        val ar = ByteArray(9)
+                        val greeting = receiveChannel.readFully(ar,0,9)
+                        if (greeting != null) {
+                            println(greeting)
+                            var outputString = ""
+                            ar.forEach { outputString+="${it.toUByte()}, " }
+                            println(outputString)
+                        } else {
+                            println("Server closed a connection")
+                            socket.close()
+                            selectorManager.close()
+                            exitProcess(0)
+                        }
+                    }
+                }
+
+                //socket.close()
             } catch (e: Exception) {
                 Log.e("ModBus", e.toString())
                 Log.e("ModBus", e.message.toString())
